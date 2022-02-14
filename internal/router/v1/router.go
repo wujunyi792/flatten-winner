e81b4d85b2fd9d445f4ccc098e03a71832628a78
Wu JunYi <wu65830600@163.com>
2022-02-14 23:53:57 +0800

    over

--- internal/router/v1/router.go
+++ internal/router/v1/router.go
@@ -1,25 +1,15 @@
 package v1
 
 import (
+	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/wujunyi792/flatten-winner/internal/handle/config"
 	"github.com/wujunyi792/flatten-winner/internal/handle/file"
-	"github.com/wujunyi792/flatten-winner/internal/response/dto"
 )
 
 func MainRouter(e *gin.Engine) {
-	e.Any("", func(c *gin.Context) {
-		res := dto.JsonResponse{}
-		res.Clear()
-		res.Data = struct {
-			UA   string
-			Host string
-		}{
-			UA:   c.Request.Header.Get("User-Agent"),
-			Host: c.Request.Host,
-		}
-		c.JSON(res.Code/100, res)
-	})
+	e.Use(static.Serve("/", static.LocalFile("./frontend", false)))
+
 	v1 := e.Group("v1")
 	v1.GET("/list", file.HandleGetList)
 	v1.POST("/set", config.HandleSetPath)
